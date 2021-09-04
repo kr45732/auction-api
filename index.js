742560ec59231fba147712710fe91ea7da40c097
kr45732 <52721908+kr45732@users.noreply.github.com>
2021-09-04 13:24:30 -0400

    YES

--- index.js
+++ index.js
@@ -2,98 +2,121 @@ const express = require('express')
 const fetch = require('node-fetch')
 const { MongoClient } = require('mongodb')
 const JSON5 = require('json5')
+const { Webhook } = require("webhook-discord")
 
 const app = express()
+const webhook = new Webhook(process.env.WEBHOOK_URL)
 
 let db
 let skyblockDB
-let config = {
-    pageSize: 20
-}
-
-let auctionHouseLoopStarted = false
 
 let pageInfo = {
-    lastUpdated: Date.now(),
-    lastPages: 0,
-    lastCompletedPages: 0,
-    lastFailedPages: 0
+    start_time: Date.now(),
+    start_time_formatted: new Date().toLocaleString('en-US', { timeZone: 'America/New_York' }),
+    last_updated: Date.now(),
+    last_updated_formatted: null,
+    last_total_pages: null,
+    last_completed_pages: null,
+    last_failed_pages: null,
+    currently_updating: false,
+    total_updates: 0,
+    authorized_requests: 0,
+    unauthorized_requests: 0
 }
 
 /* Update database */
 async function startAuctionHouseLoop() {
     while (true) {
         let ahStartTime = Date.now()
+        pageInfo.currently_updating = true
         let ah = await getFullAuctionHouse()
+        pageInfo.currently_updating = false
+        pageInfo.total_updates++
         let ahFinishTime = Date.now()
 
-        pageInfo.lastUpdated = Date.now()
-        pageInfo.lastCompletedPages = ah.completedPages.length
-        pageInfo.lastFailedPages = ah.failedPages.length
-        pageInfo.lastPages = pageInfo.lastCompletedPages + pageInfo.lastFailedPages
+        pageInfo.last_updated = Date.now()
+        pageInfo.last_completed_pages = ah.completedPages.length
+        pageInfo.last_failed_pages = ah.failedPages.length
+        pageInfo.last_total_pages = pageInfo.last_completed_pages + pageInfo.last_failed_pages
 
         if (ah.failedPages.length != 0) {
-            console.warn(`Failed to get ${ah.failedPages.length} pages. Successfully got ${ah.completedPages.length} pages`)
+            sendWebhookErrorMessage(`Failed to get ${ah.failedPages.length} pages. Successfully got ${ah.completedPages.length} pages`)
         }
 
-        console.log(`Got ${ah.auctions.length} auctions, inserting...`)
+        sendWebhookInfoMessage(`Got ${ah.auctions.length} auctions, inserting...`)
 
         let collection = skyblockDB.collection("sb")
+
+        let oldPrices = await collection.find().toArray()
+        for (const newPrice of ah.auctions) {
+            for (let i = 0; i < oldPrices.length; i++) {
+                if (oldPrices[i]["item_name"] == newPrice["item_name"]) {
+                    oldPrices.splice(i, 1)
+                }
+            }
+        }
+        oldPrices = oldPrices.concat(ah.auctions)
+
         collection.drop()
         let databaseStartTime = Date.now()
-        collection.insertMany(ah.auctions, async () => {
-            console.log(`Inserted ${ah.auctions.length} auctions in ${Date.now() - databaseStartTime}ms\n`)
+        collection.insertMany(oldPrices, async () => {
+            sendWebhookInfoMessage(`Inserted ${ah.auctions.length} auctions in ${Date.now() - databaseStartTime}ms\n`)
         })
 
-        console.log(`Total auction fetch time: ${ahFinishTime - ahStartTime}ms. Extra time: ${Date.now() - ahStartTime}ms - ${Date().toLocaleString('en-US', { timeZone: 'EST' })}`)
+        sendWebhookInfoMessage(`Total auction fetch time: ${ahFinishTime - ahStartTime}ms.\nExtra time: ${Date.now() - ahStartTime}ms`)
 
-        let nextUpdate = (await getSecondsUntilApiUpdate()) * 5000
-        console.log(`Next update in ${nextUpdate}ms\n`)
-
-        await sleep(nextUpdate)
+        await sleep(240000)
+        await sleep((await getSecondsUntilApiUpdate()) * 1000)
     }
 }
 
 /* Helper functions */
 async function getFullAuctionHouse() {
     let totalPages = (await getAuctionPage(0)).totalPages
-    console.log(`Fetching ${totalPages} pages of auction`)
+    sendWebhookInfoMessage(`Fetching ${totalPages} auction pages`)
 
     let res = await new Promise(async (resolve) => {
         let completedPages = []
         let failedPages = []
-        let ah = []
+        let newAuctionData = []
         for (let pageNum = 0; pageNum < totalPages; pageNum++) {
-            console.log(`Getting page ${pageNum}`)
             getAuctionPage(pageNum).then((page) => {
-                console.log(`then 2 - ${pageNum}`)
                 for (i of page.auctions) {
                     if (i["item_lore"].includes("Right-click to add this pet to\n§eyour pet menu") && i["bin"]) {
-                        ah.push({
-                            item_name: i["item_name"],
-                            starting_bid: i["starting_bid"],
-                            tier: i["tier"]
-                        })
+                        let itemName = `${i["item_name"]}_${i["tier"]}`.replace(/ /g, "_").toUpperCase()
+                        let price = i["starting_bid"]
+
+                        found = false
+                        for (let j = 0; j < newAuctionData.length; j++) {
+                            const element = newAuctionData[j]
+                            if (element["item_name"] == itemName) {
+                                newAuctionData[j]["starting_bid"] = Math.min(element["starting_bid"], price)
+                                found = true
+                                break
+                            }
+                        }
+
+                        if (!found) {
+                            newAuctionData.push({ item_name: itemName, starting_bid: price })
+                        }
                     }
                 }
 
                 completedPages.push(pageNum)
-                console.log(`Completed page ${pageNum}`)
-
                 if ((completedPages.length + failedPages.length) === totalPages)
                     return resolve({
                         success: true,
-                        auctions: ah,
+                        auctions: newAuctionData,
                         completedPages: completedPages,
                         failedPages: failedPages
                     })
             }).catch((e) => {
                 failedPages.push(pageNum)
-                console.error(`Failed to get page ${pageNum}, Failed ${failedPages.length} pages so far.`)
+                sendWebhookErrorMessage(`Failed to get page ${pageNum}\nFailed ${failedPages.length} pages so far\n\n\`\`\`js${e}\`\`\``)
                 if ((completedPages.length + failedPages.length) === totalPages)
                     return resolve({
                         success: true,
-                        auctions: ah,
+                        auctions: newAuctionData,
                         completedPages: completedPages,
                         failedPages: failedPages
                     })
@@ -106,7 +129,6 @@ async function getFullAuctionHouse() {
 
 async function getAuctionPage(page = 0) {
     return fetch(`https://api.hypixel.net/skyblock/auctions?page=${page}`).then((res) => {
-        console.log(`${res.ok} - ${page}`)
         if (!res.ok) {
             throw new Error(res)
         }
@@ -130,57 +152,32 @@ async function getSecondsUntilApiUpdate() {
     return maxAge - age + 2 || 50
 }
 
+async function sendWebhookInfoMessage(message) {
+    webhook.info("Auction API Logger", message)
+}
+
+async function sendWebhookErrorMessage(message) {
+    webhook.err("Auction API Logger", message)
+}
+
 /* Express App */
 app.get('/', async (req, res) => {
     res.setHeader('Content-Type', 'application/json')
 
     if (req.query.key != process.env.API_KEY) {
+        pageInfo.unauthorized_requests++
         res.status(404).send({ error: "Unauthorized" })
         return
     }
 
-    console.log(`New request: ${req.url.replace(
-        process.env.API_KEY, "[REMOVED]"
-    )}`)
+    pageInfo.authorized_requests++
+    console.log(`New request from ${req.ip}`)
 
     query = req.query.query || req.query.q || '{}'
     page = Number(req.query.page) || Number(req.query.p) || 0
     sort = req.query.sort || req.query.s || '{}'
     limit = Number(req.query.limit) || Number(req.query.l) || 9999999999999999
     filter = req.query.filter || req.query.f || '{}'
-
-    if (typeof db == 'undefined') {
-        MongoClient.connect(process.env.DATABASE_URI, { useNewUrlParser: true, useUnifiedTopology: true }, (err, DB) => {
-            db = DB
-            skyblockDB = DB.db('skyblock')
-        })
-
-        while (typeof db == 'undefined') {
-            await sleep(10)
-        }
-
-        console.log(`Successfully connected to the database\ndb: ${db}\nskyblockDB: ${skyblockDB}`)
-    }
-
-    if (!auctionHouseLoopStarted) {
-        console.log("Starting auction house loop")
-        startAuctionHouseLoop()
-        auctionHouseLoopStarted = true
-    }
-
-    if (req.query.aggregate) {
-        let aggregate
-        try {
-            aggregate = JSON5.parse(req.query.aggregate)
-        } catch (e) {
-            res.status(400).send({ error: 'Invalid JSON provided.' })
-            return
-        }
-
-        return skyblockDB.collection("sb").aggregate(aggregate).toArray().then((found) => {
-            res.json(found)
-        })
-    }
 
     try {
         query = JSON5.parse(query)
@@ -195,8 +192,8 @@ app.get('/', async (req, res) => {
         return res.status(400).json({ error: 'Invalid data type provided' })
     }
 
-    let skipSize = page * config.pageSize
-    skyblockDB.collection("sb").find(query, { allowDiskUse: true }).sort(sort).skip(skipSize).limit(req.query.page === undefined ? limit : config.pageSize).project(filter).toArray(async (err, found) => {
+    let skipSize = page * 20
+    skyblockDB.collection("sb").find(query, { allowDiskUse: true }).sort(sort).skip(skipSize).limit(req.query.page === undefined ? limit : 20).project(filter).toArray(async (err, found) => {
         if (err) {
             return res.status(500).json({ error: err })
         }
@@ -206,7 +203,8 @@ app.get('/', async (req, res) => {
 })
 
 app.get("/information", async (req, res) => {
-    res.json({ last_updated: `${(Date.now() - pageInfo.lastUpdated) / 1000.0} seconds ago`, page_info: pageInfo })
+    pageInfo.last_updated_formatted = `${(Date.now() - pageInfo.last_updated) / 1000.0} seconds ago`
+    res.json(pageInfo)
 })
 
 app.listen(process.env.PORT || 3000, async () => {
@@ -219,11 +217,7 @@ app.listen(process.env.PORT || 3000, async () => {
         await sleep(10)
     }
 
-    console.log("Successfully connected to the database. Server started. Starting auction loop.")
+    sendWebhookInfoMessage("Server started. Successfully connected to the database.Starting auction loop.")
 
-    if (!auctionHouseLoopStarted) {
-        startAuctionHouseLoop()
-    }
+    startAuctionHouseLoop()
 })
-
-module.exports = app
